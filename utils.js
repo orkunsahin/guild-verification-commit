b6f2cb16d929be539885b1d6056bd315a2d32ced
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:31:24 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:31:19.533Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-20T19:31:23.598Z
+Commit: Resolve timezone issues in date handling
