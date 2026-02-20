f2a492b959d58d048c828420b34dd7ac4b09c93b
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:31:08 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:30:16.943Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-20T19:31:07.945Z
+Commit: Patch API endpoint error handling
