e147f68be38e751759bf2546752331c341198872
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:32:01 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:31:54.686Z
-Commit: Implement user profile customization
+Timestamp: 2026-02-20T19:32:00.259Z
+Commit: Fix broken links in documentation
