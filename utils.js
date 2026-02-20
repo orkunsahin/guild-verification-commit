4eb738f920e775d2e0e8b3e5677e3f67b107afd2
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:30:17 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:10:29.719Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-02-20T19:30:16.943Z
+Commit: Fix broken links in documentation
