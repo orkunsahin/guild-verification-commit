23e45374da5f2e8b02764afdbd85958eae992243
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:31:41 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:31:30.657Z
-Commit: Add export functionality for reports
+Timestamp: 2026-02-20T19:31:41.463Z
+Commit: Patch security vulnerability in authentication
