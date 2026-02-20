14e6e56a14b992f2cbebfd59278556e249171206
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:31:28 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:31:25.885Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-20T19:31:28.285Z
+Commit: Resolve null pointer exception in user service
