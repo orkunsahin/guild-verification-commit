b0b6bcf6c0bd7c50f4229e205d3cbbd219b0f48e
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:31:25 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:31:18.048Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-02-20T19:31:24.730Z
+Commit: Fix race condition in async operations
