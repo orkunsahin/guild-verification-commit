a1881d9c5f16ad4fa4446ff7eb8c49a16f7f4fca
Orkun ŞAHİN <39224802+orkunsahin@users.noreply.github.com>
2026-02-20 22:31:43 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:31:23.598Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-02-20T19:31:42.738Z
+Commit: Fix memory leak in event handlers
